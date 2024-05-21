c8d381e5902b1c1123b3e4a7b13e9e9b00c1b765
sreemahi-code <115690831+sreemahi-code@users.noreply.github.com>
2024-05-21 15:53:51 +0400

    Update voicebotmain.py

--- voicebotmain.py
+++ voicebotmain.py
@@ -14,6 +14,7 @@ def speak(text):
 
 r = sr.Recognizer()
 
+speak("hello babe i am your voice assistant! how are you? ")
 with sr.Microphone() as source:
     r.energy_threshold=10000
     r.adjust_for_ambient_noise(source,1.2)
@@ -21,3 +22,7 @@ with sr.Microphone() as source:
     audio = r.listen(source)
     text = r.recognize_google(audio)
     print(text) 
+
+if "what" and "about" and "you" in text:
+    speak("i am having a good day")
+speak("what can i do for you?")
